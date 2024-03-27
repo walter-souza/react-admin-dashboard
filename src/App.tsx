a372059522e1ff2f9734d7dba3a7a9936bb33215
walter-souza <41118985+walter-souza@users.noreply.github.com>
2024-03-27 06:52:13 -0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,6 @@ import routerBindings, {
 import { App as AntdApp } from "antd";
 import { createClient } from "graphql-ws";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { authProvider } from "./authProvider";
-import { ColorModeContextProvider } from "./contexts/color-mode";
 
 const API_URL = "https://api.nestjs-query.refine.dev/graphql";
 const WS_URL = "wss://api.nestjs-query.refine.dev/graphql";
@@ -29,16 +27,14 @@ function App() {
   return (
     <BrowserRouter>
       <GitHubBanner />
-      <RefineKbarProvider>
-        <ColorModeContextProvider>
           <AntdApp>
             <DevtoolsProvider>
               <Refine
-                dataProvider={dataProvider(gqlClient)}
-                liveProvider={liveProvider(wsClient)}
+                // dataProvider={dataProvider(gqlClient)}
+                // liveProvider={liveProvider(wsClient)}
                 notificationProvider={useNotificationProvider}
                 routerProvider={routerBindings}
-                authProvider={authProvider}
+                // authProvider={}
                 options={{
                   syncWithLocation: true,
                   warnWhenUnsavedChanges: true,
@@ -57,8 +53,6 @@ function App() {
               <DevtoolsPanel />
             </DevtoolsProvider>
           </AntdApp>
-        </ColorModeContextProvider>
-      </RefineKbarProvider>
     </BrowserRouter>
   );
 }
